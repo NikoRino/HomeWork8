e13618ebf7cc550fe80b1735f585fe6cbff730df
NikoRino <niko89rino@gmail.com>
2023-07-30 21:28:08 +0700

    внес половину третьей и четвертой

---
+++ Lesson3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+
+void Print2Dmassive(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Create2DMassive(int rows, int columns, int startValue, int finishValue)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(startValue, finishValue);
+        }
+
+    }
+    return matrix;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInput("Введите количество строк первой матрицы: ");
+int n = GetInput("Введите количество столбцов первой матрицы: ");
+int[,] massive = Create2DMassive(m, n, 1, 10);
+
+int o = GetInput("Введите количество строк второй матрицы: ");
+int p = GetInput("Введите количество столбцов второй матрицы: ");
+int[,] massive2 = Create2DMassive(o, p, 1, 10);
+Console.WriteLine("Первая матрица");
+Print2Dmassive(massive);
+Console.WriteLine("Вторая матрица");
+Print2Dmassive(massive2);
---
+++ Lesson4/Program.cs
@@ -0,0 +1,48 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+
+// int[,,] array = { { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } } };
+
+
+
+int[,,] GetArray(int[] sizes, int min, int max)
+{
+    int[,,] array = new int[sizes[0], sizes[1], sizes[2]];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(min, max);
+
+                Console.Write($"{array[i, j, k]}({i},{j},{k})\t");
+            }
+            Console.WriteLine();
+        }
+    }
+    return array;
+}
+
+// void PrintArray(int[,,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
+
+Console.WriteLine("Введите размеры массива через пробел: ");
+string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,,] array = GetArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]), }, 10, 99);
+// PrintArray(array);
+
