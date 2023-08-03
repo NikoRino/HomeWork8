132f9c95e05d628f322d8a6cac447a9ddc22f677
NikoRino <niko89rino@gmail.com>
2023-08-03 19:26:06 +0700

    Внес задачу

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -5,8 +5,6 @@
 
 // int[,,] array = { { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } } };
 
-
-
 int[,,] GetArray(int[] sizes, int min, int max)
 {
     int[,,] array = new int[sizes[0], sizes[1], sizes[2]];
@@ -26,22 +24,7 @@ int[,,] GetArray(int[] sizes, int min, int max)
     return array;
 }
 
-// void PrintArray(int[,,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             for (int k = 0; k < array.GetLength(2); k++)
-//             {
-//                 Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
-//             }
-//             Console.WriteLine();
-//         }
-//     }
-// }
-
-Console.WriteLine("Введите размеры массива через пробел: ");
+Console.WriteLine("Введите размеры массива через пробел(три цифры): ");
 string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 int[,,] array = GetArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]), }, 10, 99);
 // PrintArray(array);
