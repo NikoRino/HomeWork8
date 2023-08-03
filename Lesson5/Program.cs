6b9556b256613f7bb91e0eb8cf1b47c790f117a2
NikoRino <niko89rino@gmail.com>
2023-08-03 20:11:59 +0700

    внес задачу

---
+++ Lesson5/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void SortArrayRowsDescend(int[,] massive)
+{
+    int sum = 1;
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            // int sum = 1;
+            // if (massive[i, j] == 0)
+            // {
+            massive[i, j] += sum;
+            sum = massive[i, j];
+            // }
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void Print2Dmassive(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Create2DMassive(int rows, int columns, int startValue, int finishValue)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = 0;
+        }
+
+    }
+    return matrix;
+}
+
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInput("Введите количество строк массива: ");
+int n = GetInput("Введите количество столбцов массива: ");
+int[,] massive = Create2DMassive(m, n, 1, 10);
+Print2Dmassive(massive);
+// Console.WriteLine("Отсортированный массив");
+SortArrayRowsDescend(massive);
+Print2Dmassive(massive);
